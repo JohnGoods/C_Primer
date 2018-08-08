343702941f1f7a06cfe3bc7db97787e7f0c72ea5
John <john@JohndeMacBook-Air.local>
2018-08-08 23:19:35 +0800

    001_HelloWork

---
+++ C_Primer/001_HelloWork/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  001_HelloWork
+//
+//  Created by John on 2018/8/8.
+//  Copyright © 2018年 C++. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
