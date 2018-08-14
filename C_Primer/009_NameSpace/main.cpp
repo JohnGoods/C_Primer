d634588f9f0a57ef0840e58041204dd9a6308787
q469672930 <469672930@qq.com>
2018-08-15 00:26:49 +0800

    namespace

---
+++ C_Primer/009_NameSpace/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  009_NameSpace
+//
+//  Created by John on 2018/8/15.
+//  Copyright © 2018年 C++. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+//using std::string;
+int main(int argc, const char * argv[]) {
+    string text = "test";
+    cout << text << endl;
+    return 0;
+}
