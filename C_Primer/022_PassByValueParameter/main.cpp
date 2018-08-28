69bf65f9b15201c7cbeeb80fce95453c899267ba
q469672930 <469672930@qq.com>
2018-08-28 21:59:13 +0800

    传值参数

--- C_Primer/022_PassByValueParameter/main.cpp
+++ C_Primer/022_PassByValueParameter/main.cpp
@@ -7,9 +7,17 @@
 //
 //传值参数
 #include <iostream>
-
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n = 0;
+    int i = n;
+    i = 42;  //i的值改变,n不会
+    //指针形参
+    int nn = 0, ii =42;
+    int *p = &nn,*q = &ii;
+    *p = 1;
+    cout << "*p" << *p <<endl;
+    p = q;
+    cout << "p" << *p <<endl;
     return 0;
 }
