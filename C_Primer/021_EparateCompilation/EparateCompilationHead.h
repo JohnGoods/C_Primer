27750c8a05d15d97a9773a5d91851d85967cfa70
q469672930 <469672930@qq.com>
2018-08-28 21:42:28 +0800

    separate compilation

---
+++ C_Primer/021_EparateCompilation/EparateCompilationHead.h
@@ -0,0 +1,15 @@
+//
+//  EparateCompilationHead.h
+//  C_Primer
+//
+//  Created by John on 2018/8/28.
+//  Copyright © 2018年 C++. All rights reserved.
+//
+
+#ifndef EparateCompilationHead_h
+#define EparateCompilationHead_h
+
+int fact(int val){
+    return val + 1;
+}
+#endif /* EparateCompilationHead_h */
---
+++ C_Primer/021_EparateCompilation/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  021_EparateCompilation
+//
+//  Created by John on 2018/8/28.
+//  Copyright © 2018年 C++. All rights reserved.
+//
+
+#include <iostream>
+#include "EparateCompilationHead.h" //头文件定义
+using namespace std;
+int main(int argc, const char * argv[]) {
+    int i = fact(1);
+    cout << "i is " << i << endl;
+    return 0;
+}
