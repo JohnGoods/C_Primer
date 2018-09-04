92d541730d1cce578296a82016784b86f64e9e94
q469672930 <469672930@qq.com>
2018-09-04 22:54:36 +0800

    有否返回函数

--- C_Primer/027_HaveOrNotReturnFunc/main.cpp
+++ C_Primer/027_HaveOrNotReturnFunc/main.cpp
@@ -18,11 +18,20 @@ void swap(int &v1,int &v2){
     v1 = tmp;
 }
 
+//直接返回
+char &get_val(string &str,string::size_type ix){
+    return str[ix];
+}
+
 int main(){
     int a = 1;
     int b = 2;
     swap(a,b);
     cout << "a is "<< a << endl;
     cout << "b is "<< b << endl;
+    string s = "a value";
+    cout << s << endl;
+    get_val(s , 0) = 'A';   //s[0] = 'A'
+    cout << s << endl;
     return 0;
 }
